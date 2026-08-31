7fd6d72e076072143eb53fa6baf4d2e13695ac8a
blackorange-sketch <mtolikm@gmail.com>
2026-08-31 16:02:00 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'barcode128-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/JsBarcode/3.11.5/JsBarcode.all.min.js',
+  'https://unpkg.com/@zxing/library@0.20.0/umd/index.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      const resClone = res.clone();
+      caches.open(CACHE).then(cache => cache.put(e.request, resClone)).catch(()=>{});
+      return res;
+    }).catch(() => cached))
+  );
+});
